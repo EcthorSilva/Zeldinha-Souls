82a2cc43550e462e7ca86349618c77f63e49c4d4
Ecthor Silva <EcthorSilva@outlook.com>
2022-05-08 19:45:02 -0300

    Feature: Camera + fake 3D (#4)

--- code/tile.py
+++ code/tile.py
@@ -1,8 +1,9 @@
 import pygame
 from settings import *
 
-class Title(pygame.sprite.Sprite):
+class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
         self.image = pygame.image.load('../Zeldinha-Souls/graphics/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0,-10)
